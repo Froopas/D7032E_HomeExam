00b9eb9c0717f63281ff7bf8a8b5fd112507a008
Anton <anton.frappe@outlook.com>
2020-10-20 10:38:08 +0200

    no qu checks in trie

--- src/Util/Trie.java
+++ src/Util/Trie.java
@@ -10,20 +10,9 @@ public class Trie {
     public void insert(String word) {
         TrieNode current = root;
         
-        for (int i = 0; i< word.length(); i++) {
-            String letter = Character.toString(word.charAt(i));
-            if ((letter.equals("Q"))||(letter.equals("q"))) {
-                letter = letter.concat("u");
-                i++;
-            }
+        for (char letter: word.toCharArray()) {
             current = current.getChildren().computeIfAbsent(letter, c -> new TrieNode());
         }
-
-        // for (char letter: word.toCharArray()) {
-        //     if ((letter == 'Q') || (letter == 'q'))
-            
-        //     current = current.getChildren().computeIfAbsent(Character.toString(letter), c -> new TrieNode());
-        // }
         current.setWord(true);
     }
 
@@ -35,12 +24,8 @@ public class Trie {
         TrieNode current = root;
 
         for (int i=0; i< word.length(); i++) {
-            String ch = Character.toString(word.charAt(i));
-            if ((ch.equals("Q"))||(ch.equals("q"))) {
-                ch = ch.concat("u");
-                i++;
-            }
-            TrieNode node = current.getChildren().get(ch.toString());
+            char ch = word.charAt(i);
+            TrieNode node = current.getChildren().get(ch);
             if (node == null) {
                 return false;
             }
@@ -62,11 +47,7 @@ public class Trie {
             current.setWord(false);
             return current.getChildren().isEmpty();
         }
-        String ch = Character.toString(word.charAt(index));
-        if ((ch.equals("Q"))||(ch.equals("q"))) {
-            ch = ch.concat("u");
-            index++;
-        }
+        char ch = word.charAt(index);
         TrieNode node = current.getChildren().get(ch);
         if (node == null) {
             return false;
--- src/Util/TrieNode.java
+++ src/Util/TrieNode.java
@@ -4,10 +4,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TrieNode {
-    private final Map<String, TrieNode> children = new HashMap<String, TrieNode>();
+    private final Map<Character, TrieNode> children = new HashMap<Character, TrieNode>();
     private boolean isWord;
 
-    public Map<String, TrieNode> getChildren() {
+    public Map<Character, TrieNode> getChildren() {
         return children;
     }
 
